eb9f1ea50b12a5927605c03ad8a9ba87a3417e19
ray-barrett <ray.barrett@gmail.com>
2014-10-05 22:39:04 +0100

    Finished euler3.go

--- euler3.go
+++ euler3.go
@@ -6,6 +6,20 @@ What is the largest prime factor of the number 600851475143 ?
 package main
 
 import "fmt"
+import "sort"
+
+type sortableArray []int
+// Methods required by sort.Interface.
+func (s sortableArray) Len() int {
+    return len(s)
+}
+// "Less" is actually greater than because I want decending order sort.
+func (s sortableArray) Less(i, j int) bool {
+    return s[i] > s[j]
+}
+func (s sortableArray) Swap(i, j int) {
+    s[i], s[j] = s[j], s[i]
+}
 
 var primes = make(map[int]bool)
 
@@ -13,7 +27,7 @@ func is_prime(num int) bool {
 	if num % 2 == 0 {
 		return false
 	}
-	for i := 3; i < (num/2)-1; i+=2 {
+	for i := 3; i < (num/i)-1; i+=2 {
 		if num % i == 0 {
 			return false
 		} 
@@ -25,7 +39,7 @@ func gen_primes(max int) {
 	if max == 2 {
 		primes[2] = true
 	} else {
-		for i := 3; i < max/2; i++ {
+		for i := 3; i < max/i-1; i += 2 {
 			if is_prime(i) {
 				primes[i] = true
 			}
@@ -33,11 +47,10 @@ func gen_primes(max int) {
 	}
 }
 
-func get_factor(num int) []int {
-	factors := make([]int, 0)
+func get_factor(num int) sortableArray {
+	factors := make(sortableArray, 0)
 	for prime := range primes {
 		if num % prime == 0 {
-			fmt.Println(prime)
 			factors = append(factors, prime)
 			if val := num/prime; val != 1 && val != 0{
 				factors = append(factors, get_factor(num / prime)...)
@@ -45,12 +58,19 @@ func get_factor(num int) []int {
 			return factors
 		}
 	}
-	return []int{num}
+	factors = append(factors, num)
+	return factors
 }
 
 func main() {
-	num := 5000
+	num := 600851475143
+	// Generate primes to factor with
 	gen_primes(num)
-	fmt.Println(primes)
-	fmt.Println(get_factor(num))
+	// Get sortableArry of factors
+	factors := get_factor(num)
+	// Sort
+	sort.Sort(factors)
+	// ??????
+	// Profit
+	fmt.Println(factors[0])
 }
